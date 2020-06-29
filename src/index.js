78bbcc44008315e25fb3e11af36564639a29023e
CZShuo <ck1000473@kimo.com>
2020-06-29 19:13:17 +0800

    Initial setting

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './index.css';
+
+const Main = (props) => {
+    
+    return (
+        <h1>Algorithm</h1>
+    );
+    
+}
+
+ReactDOM.render(<Main />,document.getElementById('root'));
